3d6e034a4ddaf10c5109c0a3b8b174b01ae39742
Nika Tsogiaidze <Tsogiaidze@yahoo.com>
2024-08-02 19:51:37 +0400

    Minimize images size

--- src/metadata.jsx
+++ src/metadata.jsx
@@ -8,7 +8,7 @@ const Metadata = () => (
   <Head>
     <title>Nika Tsogiaidze | ნიკა ცოგიაიძე</title>
     <meta name='description' content='Nika Tsogiaidze Personal Portfolio Website' />
-    <link rel='icon' sizes='32x32' href='/favicon.png' />
+    <link rel='icon' sizes='32x32' href='/favicon.jpg' />
     <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
     <meta name='viewport' content='width=device-width, initial-scale=1' />
   </Head>
