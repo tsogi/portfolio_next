729e1e8ad8cebf1dda201c0889a4d1f500cd683c
Nika Tsogiaidze <Tsogiaidze@yahoo.com>
2024-12-18 12:35:45 +0400

    Update CV and deployment

--- deploy_to_acer.sh
+++ deploy_to_acer.sh
@@ -1,3 +1,5 @@
+npm run build
+
 docker buildx build --platform linux/amd64 -t portfolio:1.1 --output "type=docker,dest=./portfolio.tar" .
 
 scp -i /users/nika/.ssh/id_rsa_ubuntu_server ./portfolio.tar nika@tsogi.net:/var/projects
@@ -7,12 +9,11 @@ ssh -i /users/nika/.ssh/id_rsa_ubuntu_server nika@tsogi.net << EOF
     # Navigating to the directory
     cd /var/projects
     
-    # Executing the commands
-    docker load -i portfolio.tar
-    docker-compose rm -f portfolio
-    docker-compose up -d portfolio
 
-    # Delete all unused containers, networks, images
+    docker-compose stop portfolio
+    docker-compose rm -f portfolio
+    docker load -i portfolio.tar
+    docker-compose up -d portfolio
     docker system prune -f
 
 EOF
